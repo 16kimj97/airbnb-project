af23653f9a6a1d6f37a6a4ac4927be2732104d39
Justin <16kimj97@gmail.com>
2024-02-23 10:41:53 -0800

    please mom

--- backend/db/migrations/20240223170223-addFirstNameAndLastNameToUsers.js
+++ backend/db/migrations/20240223170223-addFirstNameAndLastNameToUsers.js
@@ -1,21 +1,41 @@
-'use strict';
+"use strict";
+
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Users', 'firstName', {
-      type: Sequelize.STRING,
-      allowNull: false,
-    }, options);
-
-    await queryInterface.addColumn('Users', 'lastName', {
-      type: Sequelize.STRING,
-      allowNull: false,
-    }, options);
+  async up(queryInterface, Sequelize) {
+    await User.bulkCreate(
+      [
+        {
+          email: "demo@user.io",
+          username: "Demo-lition",
+          hashedPassword: bcrypt.hashSync("password"),
+          firstName: "Demo",
+          lastName: "User",
+        },
+        {
+          email: "user1@user.io",
+          username: "FakeUser1",
+          hashedPassword: bcrypt.hashSync("password2"),
+          firstName: "User",
+          lastName: "One",
+        },
+        {
+          email: "user2@user.io",
+          username: "FakeUser2",
+          hashedPassword: bcrypt.hashSync("password3"),
+          firstName: "User",
+          lastName: "Two",
+        },
+      ],
+      { validate: true }
+    );
   },
 
   async down(queryInterface, Sequelize) {
