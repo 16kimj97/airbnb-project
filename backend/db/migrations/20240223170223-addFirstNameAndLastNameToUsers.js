37b9660fc15ea40e19fb2ec2ff43613b725ee991
Justin <16kimj97@gmail.com>
2024-02-23 10:43:27 -0800

    this one

--- backend/db/migrations/20240223170223-addFirstNameAndLastNameToUsers.js
+++ backend/db/migrations/20240223170223-addFirstNameAndLastNameToUsers.js
@@ -1,52 +1,50 @@
 "use strict";
 
-const { User } = require("../models");
-const bcrypt = require("bcryptjs");
-
 let options = {};
 if (process.env.NODE_ENV === "production") {
   options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await User.bulkCreate(
-      [
-        {
-          email: "demo@user.io",
-          username: "Demo-lition",
-          hashedPassword: bcrypt.hashSync("password"),
-          firstName: "Demo",
-          lastName: "User",
-        },
-        {
-          email: "user1@user.io",
-          username: "FakeUser1",
-          hashedPassword: bcrypt.hashSync("password2"),
-          firstName: "User",
-          lastName: "One",
-        },
-        {
-          email: "user2@user.io",
-          username: "FakeUser2",
-          hashedPassword: bcrypt.hashSync("password3"),
-          firstName: "User",
-          lastName: "Two",
-        },
-      ],
-      { validate: true }
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    const table = { tableName: "Users", ...options };
+
+    await queryInterface.addColumn(
+      table,
+      "firstName",
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      options
+    );
+    await queryInterface.addColumn(
+      table,
+      "lastName",
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      options
     );
   },
 
   async down(queryInterface, Sequelize) {
-    options.tableName = "Users";
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(
-      options,
-      {
-        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
-      },
-      {}
-    );
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    const table = { tableName: "Users", ...options };
+    await queryInterface.removeColumn(table, "firstName");
+    await queryInterface.removeColumn(table, "lastName");
   },
 };
