6a5630ad0a943f6b2686bdae0486f312eb3affa6
Justin <16kimj97@gmail.com>
2024-02-25 15:21:35 -0800

    create-review table

---
+++ backend/db/models/reviewimages.js
@@ -0,0 +1,36 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+    class ReviewImage extends Model {
+        static associate(models) {
+          ReviewImage.belongsTo(models.Review, {
+            foreignKey: 'reviewId',
+            onDelete: 'CASCADE'
+        });
+    }
+};
+
+  ReviewImage.init({
+    reviewId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Review',
+        key: 'id'
+      }
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'ReviewImage',
+  });
+
+  return ReviewImage;
+};
