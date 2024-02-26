6075faa2d84a64a5ba4c13cb5bd47f091f56c781
Justin <16kimj97@gmail.com>
2024-02-25 16:38:01 -0800

    create-spots-edited

--- backend/db/migrations/20240225185002-create-spots.js
+++ backend/db/migrations/20240225185002-create-spots.js
@@ -1,5 +1,11 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Spots', {
@@ -61,16 +67,36 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
+
+    // Alter Users table to add ownerId column
+    await queryInterface.addColumn(
+      'Users',
+      'ownerId',
+      {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      }
+    );
   },
 
   async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'ownerId');
+
     await queryInterface.dropTable('Spots');
   }
 };
