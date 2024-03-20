edc650146988e1a7747977c538cf11505a016ba2
Justin <16kimj97@gmail.com>
2024-03-20 15:33:08 -0700

    action creator for create spot

---
+++ frontend/src/store/components/CreateSpot/CreateSpot.jsx
@@ -0,0 +1,5 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useState, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { createNewSpot } from "../../spots"
+import './CreateSpot.css'
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -3,6 +3,7 @@ import { csrfFetch } from "./csrf";
 // Action Types
 const FETCH_SPOTS = 'spots/FETCH_SPOTS';
 const GET_SPOT_BY_ID = 'spots/GET_SPOT_BY_ID';
+export const CREATE_SPOT = 'spots/CREATE_SPOT';
 
 // Action Creator For Fetching Spots
 const fetchSpotsSuccess = (spots) => ({
@@ -14,6 +15,13 @@ const getSpotByIdSuccess = (spot) => ({
   type: GET_SPOT_BY_ID,
   payload: spot
 });
+//CreateSpot Action creator
+export const createSpotSuccess = (spot) => {
+    return {
+        type: CREATE_SPOT,
+        payload: spot,
+    };
+};
 
 
 // Thunk Action fetchspots
@@ -46,32 +54,64 @@ export const fetchSpotById = (spotId) => async (dispatch) => {
   }
 };
 
+//Create a spot
+export const createNewSpot = (spot) => async(dispatch) => {
+    const response = await csrfFetch('/api/spots', {
+        method: 'POST',
+        body: JSON.stringify(spot)
+    })
+    if(response.ok){
+        const data = await response.json();
+        dispatch(viewSpot(data.id))
+
+        spot.SpotImages.map(img => {
+            csrfFetch(`/api/spots/${data.id}/images`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    url: img.url
+                })
+            })
+        })
+        return data;
+    }
+    if(!response.ok){
+        throw new Error('Error creating new spot')
+    }
+}
+
+
 
 // initial
 const initialState = { spot: null, spots: [] };
 
 //Reducer
 function spotReducer(state = initialState, action) {
-  switch (action.type) {
+    switch (action.type) {
       case FETCH_SPOTS: {
-          const spots = {};
-          action.payload.Spots.forEach(spot => {
-              spots[spot.id] = spot;
-          });
-          return {
-              ...state,
-              spots
-          };
+        const spots = {};
+        action.payload.Spots.forEach(spot => {
+          spots[spot.id] = spot;
+        });
+        return {
+          ...state,
+          spots
+        };
       }
       case GET_SPOT_BY_ID: {
-          return {
-              ...state,
-              spot: action.payload
-          };
+        return {
+          ...state,
+          spot: action.payload
+        };
+      }
+      case CREATE_SPOT: {
+        return {
+          ...state,
+          spots: [...state.spots, action.payload]
+        };
       }
       default:
-          return state;
+        return state;
+    }
   }
-}
 
 export default spotReducer;
