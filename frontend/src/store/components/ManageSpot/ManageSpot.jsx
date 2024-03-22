1f241fb5a65ed33aa0e39282a093868bcfead621
Justin <16kimj97@gmail.com>
2024-03-22 14:22:34 -0700

    createbutton working

--- frontend/src/store/components/ManageSpot/ManageSpot.jsx
+++ frontend/src/store/components/ManageSpot/ManageSpot.jsx
@@ -2,14 +2,16 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { getSpotByUserId } from '../../spots';
 import SpotTile from '../LandingPage/SpotTile'
+import './ManageSpot.css'
+import { useNavigate } from 'react-router-dom'
 
 const ManageSpotPage = () => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user);
     const spots = useSelector((state) => Object.values(state.spots.spots));
+    const navigate = useNavigate();
 
-    console.log(spots);
-
+    // console.log(spots);
 
     useEffect(() => {
         if (user) {
@@ -19,14 +21,28 @@ const ManageSpotPage = () => {
 
     const flattenedSpots = spots.flat();
 
+    const isSpotEmpty = () => flattenedSpots.length === 0;
+
     return (
         <div>
             <h1>Manage Your Spots</h1>
-            <div className="spot-list">
-                {flattenedSpots.map(spot => (
-                    <SpotTile key={spot.id} spot={spot} />
-                ))}
-            </div>
+            {!isSpotEmpty() ? (
+                <div className="spot-list">
+                    {flattenedSpots.map(spot => (
+                        <SpotTile key={spot.id} spot={spot} />
+                    ))}
+                </div>
+            ) : (
+                <div className="no-spots-message">
+                    <p>No spots have been posted yet.</p>
+                    <button
+                      className="create-new-spot-button"
+                      onClick={() => navigate('/spots/new')}
+                    >
+                        Create a New Spot
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
