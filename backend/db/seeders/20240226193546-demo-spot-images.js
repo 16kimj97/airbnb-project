83fcd93fe137e27ed9497d6c52a4957ae4f54654
Justin <16kimj97@gmail.com>
2024-03-24 14:09:35 -0700

    more pictures in db

--- backend/db/seeders/20240226193546-demo-spot-images.js
+++ backend/db/seeders/20240226193546-demo-spot-images.js
@@ -54,32 +54,82 @@ module.exports = {
       },
       {
         spotId: 1,
-        url: "https://i.postimg.cc/65dQr5hm/wano.png",
+        url: "https://i.postimg.cc/DzVV6FSV/wano-2.jpg",
         preview: false,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         spotId: 1,
-        url: "https://i.postimg.cc/65dQr5hm/wano.png",
+        url: "https://i.postimg.cc/3R1m98Jn/wano-3.jpg",
         preview: false,
         createdAt: new Date(),
         updatedAt: new Date()
       }, {
         spotId: 1,
-        url: "https://i.postimg.cc/65dQr5hm/wano.png",
+        url: "https://i.postimg.cc/jSBpdKTg/wano-4.jpg",
         preview: false,
         createdAt: new Date(),
         updatedAt: new Date()
       }, {
         spotId: 1,
-        url: "https://i.postimg.cc/65dQr5hm/wano.png",
+        url: "https://i.postimg.cc/8k6Bq6Pr/wano-5.jpg",
         preview: false,
         createdAt: new Date(),
         updatedAt: new Date()
-      }, {
-        spotId: 1,
-        url: "https://i.postimg.cc/65dQr5hm/wano.png",
+      },
+      {
+        spotId: 2,
+        url: "https://i.postimg.cc/Vs4Rm8Pj/dressrosa-2.jpg",
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        spotId: 2,
+        url: "https://i.postimg.cc/DZ0zGLmf/dressrosa-3.jpg",
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        spotId: 2,
+        url: "https://i.postimg.cc/vHNC89wL/dressrosa-4.png",
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        spotId: 2,
+        url: "https://i.postimg.cc/x1WCmcML/dressrosa-5.png",
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        spotId: 3,
+        url: "https://i.postimg.cc/zXw6KfM4/water-2.jpg",
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        spotId: 3,
+        url: "https://i.postimg.cc/ZRCFYftJ/water-3.png",
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        spotId: 3,
+        url: "https://i.postimg.cc/Y0ScSB2h/water-4.jpg",
+        preview: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        spotId: 3,
+        url: "https://i.postimg.cc/MKk44BVn/water-5.jpg",
         preview: false,
         createdAt: new Date(),
         updatedAt: new Date()
